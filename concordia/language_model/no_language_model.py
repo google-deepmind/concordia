290265a3e9cc94659112b5035251d367e2d18509
Joel Z. Leibo <jzl@google.com>
2024-05-15 08:49:30 -0700

    Add a debug language model that always returns empty strings and choice 0.
    PiperOrigin-RevId: 633969613
    Change-Id: I0c84a721dbbb84259a5b383cf26e7b88b569a212

---
+++ concordia/language_model/no_language_model.py
@@ -0,0 +1,55 @@
+# Copyright 2023 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Language model that always returns empty strings and choice 0 (for debug)."""
+
+from collections.abc import Collection, Mapping, Sequence
+from typing import Any
+
+from concordia.language_model import language_model
+from typing_extensions import override
+
+
+class NoLanguageModel(language_model.LanguageModel):
+  """Debuging model that always returns empty strings and choice 0."""
+
+  def __init__(
+      self,
+  ) -> None:
+    """Debuging model that always returns empty strings and choice 0."""
+  pass
+
+  @override
+  def sample_text(
+      self,
+      prompt: str,
+      *,
+      max_tokens: int = language_model.DEFAULT_MAX_TOKENS,
+      max_characters: int = language_model.DEFAULT_MAX_CHARACTERS,
+      terminators: Collection[str] = language_model.DEFAULT_TERMINATORS,
+      temperature: float = language_model.DEFAULT_TEMPERATURE,
+      timeout: float = language_model.DEFAULT_TIMEOUT_SECONDS,
+      seed: int | None = None,
+  ) -> str:
+    return ''
+
+  @override
+  def sample_choice(
+      self,
+      prompt: str,
+      responses: Sequence[str],
+      *,
+      seed: int | None = None,
+  ) -> tuple[int, str, Mapping[str, Any]]:
+    return 0, 'yes', {}
