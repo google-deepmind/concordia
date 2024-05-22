f49c448315caace4bfa370257bb27a8cfa38df9f
Joel Z. Leibo <jzl@google.com>
2024-05-22 09:04:49 -0700

    Add default importance and clock to blank memory factory.
    PiperOrigin-RevId: 636186811
    Change-Id: I98bc0afe2a28d8cc4e62b854fbc81af7f8a3f810

--- concordia/associative_memory/blank_memories.py
+++ concordia/associative_memory/blank_memories.py
@@ -19,6 +19,7 @@ from collections.abc import Callable
 import datetime
 
 from concordia.associative_memory import associative_memory
+from concordia.associative_memory import importance_function
 from concordia.language_model import language_model
 import numpy as np
 
@@ -30,13 +31,24 @@ class MemoryFactory:
       self,
       model: language_model.LanguageModel,
       embedder: Callable[[str], np.ndarray],
-      importance: Callable[[str], float],
-      clock_now: Callable[[], datetime.datetime],
+      importance: Callable[[str], float] | None = None,
+      clock_now: Callable[[], datetime.datetime] | None = None,
   ):
+    """Initializes the memory factory.
+
+    Args:
+      model: The language model to use
+      embedder: The text embedder to use
+      importance: maps a sentence into [0, 1] scale of importance, if None then
+        use a constant importance model that sets all memories to importance 1.0
+      clock_now: a callable to get time when adding memories, if None then use
+        the current time.
+    """
     self._model = model
     self._embedder = embedder
-    self._importance = importance
-    self._clock_now = clock_now
+    self._importance = (
+        importance or importance_function.ConstantImportanceModel())
+    self._clock_now = clock_now or datetime.datetime.now
 
   def make_blank_memory(
       self,
