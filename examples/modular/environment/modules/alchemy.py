f98c2a6c0bd99bdcc0e2bedd9ca013793eafdbdc
Joel Z. Leibo <jzl@google.com>
2024-06-03 04:45:11 -0700

    Add another modular example. This one is set in Victorian London.
    PiperOrigin-RevId: 639735424
    Change-Id: I87b99792a671d948fc2a229ab8e5787f9bb3e2a5

---
+++ examples/modular/environment/modules/alchemy.py
@@ -0,0 +1,61 @@
+# Copyright 2024 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A set of statements about alchemy which can be used to seed background."""
+
+# These world building elements were drawn from the following book:
+#    Roob, Alexander. (2021). Alchemy & Mysticism. Taschen.
+# They are not exact quotes from the book, and the statements, such as they are,
+# do not accurately portray alchemical "knowledge" (JZL permuted their words and
+# ideas).
+ITEMS = (
+    (
+        'The wise sages have taught that the region is full of darkness on '
+        'account of an incorrect application of Joachim of Fiore\'s doctrines '
+        'concerning Sal Alkali, the light of nature, a green dragon, and a '
+        'philosopher\'s mercury. Only one as pure as a child may discover the '
+        'treasure of Sal Alkali.'
+    ),
+    (
+        'Nine philosophers on a ladder may embody the nine doubts in a '
+        'nine-object realm of the universe. A ladder with thirty rungs '
+        'represents the thirty virtues declared by John Kilmakos of Sinai.'
+    ),
+    (
+        'The Lunar Vulcan is a secret fire formed by salts of the dew. '
+        'It is said that once perfected, this fire may be able to revive '
+        'the dead. It must be entrusted to digestion.'
+    ),
+    (
+        'According to the Rosarium Philosophorum, the pelican feeds its young '
+        'with blood. It symbolizes the final phase of the Multiplicatio.'
+    ),
+    (
+        'The immortal Rebis may rise as one from a putrified grave, attended '
+        'by ravens and adorned in philosophical gold and silver. Ulmannus '
+        'spoke of an inward, spiritual sun embodying the divine Caduceus.'
+    ),
+    (
+        'In the Ars Magna Lucis et Umbrae it is explained how the ladder of '
+        'the number ten is divided and inscribed with the names of the '
+        'Sefirot. In contrast, the Opus Magnum depicts the Sefirot in '
+        'association with a tree, not a ladder.'
+    ),
+    (
+        'Isidore of Seville found the Aurora Consurgens, and in its pages, '
+        'he discovered the most remarkable truth: that a thing cannot be '
+        'changed into another of a different nature without first being '
+        'transformed into ash, lime, earth, or bile.'
+    ),
+)
