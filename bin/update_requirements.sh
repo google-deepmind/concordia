93095a46bba776082f52036783316a00997122bf
John Agapiou <jagapiou@google.com>
2025-04-24 11:01:35 -0700

    Update requirements.txt and add pip and setuptools
    PiperOrigin-RevId: 751057085
    Change-Id: I63f357fa8de0d3eaed494493ab278f484b4ff59a

--- bin/update_requirements.sh
+++ bin/update_requirements.sh
@@ -18,5 +18,5 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-pip-compile --generate-hashes --reuse-hashes --strip-extras \
+pip-compile --generate-hashes --reuse-hashes --strip-extras  --allow-unsafe \
      --upgrade --extra dev setup.py examples/requirements.in
