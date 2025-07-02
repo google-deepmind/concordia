7bf36104560acc0b9e31eb11519d7f24896562d1
Logan Cross <lcross@google.com>
2025-07-02 03:04:28 -0700

    Minor: Update game master prefabs imports
    PiperOrigin-RevId: 778422597
    Change-Id: I3948d52aeefcf8f969fe90c41bffc3f295ee3c30

--- concordia/prefabs/game_master/__init__.py
+++ concordia/prefabs/game_master/__init__.py
@@ -20,4 +20,6 @@ from concordia.prefabs.game_master import formative_memories_initializer
 from concordia.prefabs.game_master import game_theoretic_and_dramaturgic
 from concordia.prefabs.game_master import generic
 from concordia.prefabs.game_master import interviewer
+from concordia.prefabs.game_master import marketplace
+from concordia.prefabs.game_master import psychology_experiment
 from concordia.prefabs.game_master import situated
