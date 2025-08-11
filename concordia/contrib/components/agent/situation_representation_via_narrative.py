50fe3b2d0b2da3b897400be06d666d1c99e0a2e5
Joel Z. Leibo <jzl@google.com>
2025-08-11 02:02:34 -0700

    Modernize the situation_representation_via_narrative component.
    PiperOrigin-RevId: 793546033
    Change-Id: Ia0009d189a275057429eb3d124cbed406b168ad6

--- concordia/contrib/components/agent/situation_representation_via_narrative.py
+++ concordia/contrib/components/agent/situation_representation_via_narrative.py
@@ -15,9 +15,8 @@
 """A component for representing the current situation via narrative.
 """
 
-from collections.abc import Callable, Mapping
+from collections.abc import Callable, Sequence
 import datetime
-import types
 
 from concordia.components.agent import action_spec_ignored
 from concordia.document import interactive_document
@@ -32,12 +31,10 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
   def __init__(
       self,
       model: language_model.LanguageModel,
-      clock_now: Callable[[], datetime.datetime],
       observation_component_key: str,
-      components: Mapping[
-          entity_component.ComponentName, str
-      ] = types.MappingProxyType({}),
+      components: Sequence[str] = (),
       declare_entity_as_protagonist: bool = True,
+      clock_now: Callable[[], datetime.datetime] | None = None,
       pre_act_label: str = 'The current situation',
       logging_channel: logging.LoggingChannel = logging.NoOpLoggingChannel,
   ):
@@ -45,13 +42,12 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
 
     Args:
       model: The language model to use.
-      clock_now: Function that returns the current time.
       observation_component_key: The name of the component that contains the
         latest observations.
-      components: Components to condition the narrative on. This is a mapping
-        of the component name to a label to use in the prompt.
+      components: Keys of components to condition the pre_act value on.
       declare_entity_as_protagonist: Whether to declare the entity to be the
         protagonist in the prompt.
+      clock_now: Optional function that returns the current time.
       pre_act_label: Prefix to add to the output of the component when called
         in `pre_act`.
       logging_channel: The channel to log debug information to.
@@ -82,7 +78,9 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
   def _make_pre_act_value(self) -> str:
     """Returns a representation of the current situation to pre act."""
     agent_name = self.get_entity().name
-    current_time = self._clock_now()
+    current_time = ''
+    if self._clock_now is not None:
+      current_time = self._clock_now()
     observations = self.get_entity().get_component(
         self._observation_component_key)
 
@@ -95,7 +93,8 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
       mems = observations.get_pre_act_value()
       chain_of_thought.statement(f'Story fragments and world data:\n{mems}')
       self._add_components_if_any(chain_of_thought)
-      chain_of_thought.statement(f'Events continue after {current_time}')
+      if self._clock_now is not None:
+        chain_of_thought.statement(f'Events continue after {current_time}')
       self._situation_thus_far = chain_of_thought.open_question(
           question=(
               'Narratively summarize the story fragments and world data. Give '
@@ -120,7 +119,7 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
     if self._declare_entity_as_protagonist:
       chain_of_thought.statement(f'Protagonist: {agent_name}')
       chain_of_thought.statement(
-          f'Thoughts and memories of {agent_name}:\n{result}'
+          f'Observations, thoughts, and memories of {agent_name}:\n{result}'
       )
     else:
       chain_of_thought.statement(f'Notes:\n{result}')
@@ -140,7 +139,8 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
         terminators=(),
         question_label='Exercise',
     )
-    chain_of_thought.statement(f'The current date and time is {current_time}')
+    if self._clock_now is not None:
+      chain_of_thought.statement(f'The current date and time is {current_time}')
 
     chain_of_thought_text = '\n'.join(
         chain_of_thought.view().text().splitlines())
@@ -153,29 +153,16 @@ class SituationRepresentation(action_spec_ignored.ActionSpecIgnored):
                              chain_of_thought_text),
     })
 
-    self._previous_time = current_time
-
     return self._situation_thus_far
 
   def get_state(self) -> entity_component.ComponentState:
     """Converts the component to JSON data."""
     with self._lock:
-      if self._previous_time is None:
-        previous_time = ''
-      else:
-        previous_time = self._previous_time.strftime('%Y-%m-%d %H:%M:%S')
       return {
-          'previous_time': previous_time,
           'situation_thus_far': self._situation_thus_far,
       }
 
   def set_state(self, state: entity_component.ComponentState) -> None:
     """Sets the component state from JSON data."""
     with self._lock:
-      if state['previous_time']:
-        previous_time = datetime.datetime.strptime(
-            state['previous_time'], '%Y-%m-%d %H:%M:%S')
-      else:
-        previous_time = None
-      self._previous_time = previous_time
       self._situation_thus_far = state['situation_thus_far']
