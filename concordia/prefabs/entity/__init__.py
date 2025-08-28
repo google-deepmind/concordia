67a4e40ca298524dee78d86079ee3172a53e0312
Logan Cross <lcross@google.com>
2025-08-28 04:13:40 -0700

    An agent that generates engaging conversation
    PiperOrigin-RevId: 800404445
    Change-Id: Iadd1d0784967ddbd844e1232c235eb393b5f8f38

--- concordia/prefabs/entity/__init__.py
+++ concordia/prefabs/entity/__init__.py
@@ -17,5 +17,6 @@
 from concordia.prefabs.entity import basic
 from concordia.prefabs.entity import basic_scripted
 from concordia.prefabs.entity import basic_with_plan
+from concordia.prefabs.entity import conversational
 from concordia.prefabs.entity import fake_assistant_with_configurable_system_prompt
 from concordia.prefabs.entity import minimal
