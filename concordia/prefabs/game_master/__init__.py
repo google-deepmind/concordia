3da5bdf56e132522e731898104f74b0619df0c38
Sasha Vezhnevets <vezhnick@google.com>
2026-02-12 06:18:45 -0800

    The scene tracker now queues premises on the first action of step 0, using a memory tag to ensure it happens only once per scene.
    This addresses issues where premises were not being added correctly when used with the initializer GM.
    Removing the dramaturgic initializer as the new scene tracker solves the issue.
    
    PiperOrigin-RevId: 869182124
    Change-Id: I5950bb84f80ec59a56f1c3a7941c328c9011d0cf

--- concordia/prefabs/game_master/__init__.py
+++ concordia/prefabs/game_master/__init__.py
@@ -17,7 +17,6 @@
 from concordia.prefabs.game_master import dialogic
 from concordia.prefabs.game_master import dialogic_and_dramaturgic
 from concordia.prefabs.game_master import formative_memories_initializer
-from concordia.prefabs.game_master import formative_memories_initializer_dramaturgic
 from concordia.prefabs.game_master import game_theoretic_and_dramaturgic
 from concordia.prefabs.game_master import generic
 from concordia.prefabs.game_master import interviewer
